cb78530344ac48c12061e1e50f8a28ddc881a87d
Marwan <marwan@Marwans-MacBook-Pro.local>
2026-07-25 22:15:25 -0600

    init

---
+++ src/screens/DeviceScanScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function DeviceScanScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Device Scan Screen</Text>
+    </View>
+  );
+}
